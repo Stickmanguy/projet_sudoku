b220d4e37b5f13d1770a699b53edd25b5b6bc0e0
brouettelover <59088394+brouettelover@users.noreply.github.com>
2020-04-19 17:48:26 +0200

    ff

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from create_sudoku import CreateSudoku
 from ajout_de_valeur import *
 from ajout_utilisateur import *
+from verif_utilisateur import *
 import modes
-sudoku_01 = CreateSudoku()
 
-modes.mort_subite(sudoku_01.data)
 
